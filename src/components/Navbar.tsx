7880dc1b73832c736cb3c6cccd71d62b057a6965
Antwannnn <jeanpierrepernette@gmail.com>
2025-08-27 10:59:27 +0000

    little border problem nique ta mere

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -151,7 +151,7 @@ export default function Navbar() {
   }, [isMenuOpen])
 
   return (
-    <nav className="sticky top-0 left-0 z-50 w-full bg-background border-b border-border/20">
+    <nav className="sticky top-0 left-0 z-50 w-full bg-background border-b border-accent">
       <div className="w-full px-4 md:px-6">
         <div className="relative flex justify-between items-center h-16">
           {/* Logo */}
